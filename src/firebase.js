f8ad64664da77b360d49a04ddf2c5c08721c12b2
SiddharthaMishra-dev <mailbox.siddharthamishra@gmail.com>
2023-02-26 01:11:56 +0530

    update files

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import {getAuth} from 'firebase/auth'
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig={
-    apiKey: "AIzaSyBOlw1m0TRD9C9VpRik0-_ZxdbQnb3ddmQ",
-    authDomain: "todo-f4ab7.firebaseapp.com",
-    projectId: "todo-f4ab7",
-    storageBucket: "todo-f4ab7.appspot.com",
-    messagingSenderId: "91492208999",
-    appId: "1:91492208999:web:64b96d3e6bab252b7e1fc2",
-    measurementId: "G-XFZSR9Y782"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET ,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId:process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 const app=initializeApp(firebaseConfig);
 const db=getFirestore(app)
